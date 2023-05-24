78fcd9f4bafb016487101ce19ac7d3d51cb9ff90
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2023-05-24 05:23:52 -0600

    Update app.py

--- app.py
+++ app.py
@@ -13,5 +13,11 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
   reader = PdfReader(uploaded_file)
   st.write(len(reader.pages))
+  page = reader.pages
+  full_text=[]
+  for p in page:
+    text = p.extract_text()
+    full_text.append(text)
+    full_text
   
 llm = OpenAI(temperature=0)
